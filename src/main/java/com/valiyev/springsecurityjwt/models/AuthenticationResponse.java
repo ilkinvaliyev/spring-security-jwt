ad7825cb190472f097935f0656be719390e6410e
Ilkin Valiyev <ilkinv1994@gmail.com>
2021-10-13 13:09:25 +0400

    Initial commit

---
+++ src/main/java/com/valiyev/springsecurityjwt/models/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.valiyev.springsecurityjwt.models;
+
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
